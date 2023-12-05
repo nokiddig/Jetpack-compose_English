859d79e16f0374c8ab1771a4386a46e0b48de4a5
HP <sy2000dn0@gmail.com>
2023-12-05 17:02:27 +0700

    [architecture] fix repository

--- app/src/main/java/com/example/english_app/di/AppModule.kt
+++ app/src/main/java/com/example/english_app/di/AppModule.kt
@@ -1,5 +1,6 @@
 package com.example.english_app.di
 
+import com.example.english_app.data.datasource.AccountDataSource
 import com.example.english_app.data.repository.AccountRepository
 import com.example.english_app.domain.usecase.LoginUseCase
 import com.example.english_app.presentation.login.viewmodel.LoginViewModel
@@ -9,19 +10,19 @@ import com.google.firebase.auth.FirebaseAuth
 import javax.inject.Singleton
 
 
-//class AppModule {
-//    @Provides
-//    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
-//
-//    @Provides
-//    fun provideUserRepository(dataSource: FirebaseAuthDataSource): UserRepository {
-//        return UserRepositoryImpl(dataSource)
-//    }
-//
-//    // Các provides khác...
-//}
-//
-//@Component(modules = [AppModule::class])
-//interface AppComponent {
-//    // Đăng ký các phương thức inject cho các thành phần khác...
-//}
+class AppModule {
+    @Provides
+    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+
+    @Provides
+    fun provideUserRepository(dataSource: AccountDataSource): AccountRepository {
+        return AccountRepository(dataSource)
+    }
+
+    // Các provides khác...
+}
+
+@Component(modules = [AppModule::class])
+interface AppComponent {
+    // Đăng ký các phương thức inject cho các thành phần khác...
+}
--- app/src/main/java/com/example/english_app/domain/usecase/LoginUseCase.kt
+++ app/src/main/java/com/example/english_app/domain/usecase/LoginUseCase.kt
@@ -1,6 +1,6 @@
 package com.example.english_app.domain.usecase
 
-import com.example.english_app.domain.repository.AccountRepository
+import com.example.english_app.data.repository.AccountRepository
 import com.google.firebase.firestore.auth.User
 
 class LoginUseCase(private val accountRepository: AccountRepository) {
